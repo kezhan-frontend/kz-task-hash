f29a731e93600aea665afab5435491a2e7f60d25
wyicwx <wyicwx@gmail.com>
2016-04-14 10:59:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# kz-task-hash
+
+```js
+var hash = require('kz-task-hash');
+
+bone.task('hash', hash({
+    distPath: '',
+    versionPath: ''
+}))
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = function(options) {
+    options || (options = {});
+
+    if(!options.distPath || !options.versionPath) {
+        throw new Error('options.distPath & options.version are required!');
+    }
+
+    return function() {
+        var fs = require('fs');
+        var md5 = require('MD5');
+        var version = {};
+        var bonefs = this.fs;
+        var files = bonefs.search(options.distPath'/**/*.*');
+        var distPath = bonefs.pathResolve(options.distPath);
+        var path = require('path');
+        var ignoreExt = {'.json': true};
+
+        bone.log('正在计算hash值！');
+
+        files.forEach(function(file) {
+            // filter image
+            if(path.extname(file) in ignoreExt)
+                return;
+            var ctn = fs.readFileSync(file);
+            var p = file.replace(distPath+'/', '/');
+
+            version[p] = md5(ctn).substr(-6);
+        });
+        console.log('写入version.json文件.');
+        bonefs.writeFile(options.versionPath, JSON.stringify(version, null, 4));
+    }
+};
